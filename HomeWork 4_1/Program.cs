40a53854b1916aa533a5fc06c396cf092a13f86d
Владимир Александрович Кирдяшкин <kva@vast.su>
2022-11-09 18:30:44 +0300

    123

--- HomeWork 4_1/Program.cs
+++ HomeWork 4_1/Program.cs
@@ -10,11 +10,11 @@ namespace HomeWork_4_1
     {
         static void Main(string[] args)
         {
-            Random random = new Random(); // резерв
-            int[] box = new int[random.Next(5, 50)]; // 
+            Random random = new Random(); 
+            int[] box = new int[random.Next(5, 50)]; 
             int maxInd = 0, minInd = 0;
             for(int i = 0; i < box.Length; i++)
-                box[i] = random.Next(100, 400); // присвоить каждой ячейки значение
+                box[i] = random.Next(100, 400); 
             int min = box[0];
             int max = box[0];
             int avg = 0;
@@ -34,8 +34,13 @@ namespace HomeWork_4_1
                 Console.WriteLine(box[i]);
             }
             avg /= box.Length;
+            double minDif = max;
+            int minDifInd = 0;
             for (int i = 0; i < box.Length; i++)
                 if (box[i] < avg == 1)
+                    double dif = avg - box[i];
+
+            if (box[i] < avg == 1)
 
             Console.WriteLine($"{box[maxInd]} = {maxInd} {box[minInd]} = {minInd}");
             Console.ReadLine();
